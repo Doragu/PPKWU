5c3e14b7d048f55422c1d15a0c1128767b2bc4d8
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-10-13 14:51:21 +0200

    ZAD1 Made /rev route return text passed as variable in URL

--- zad1/app.py
+++ zad1/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/rev')
-def revert_string():
-    return ""
+@app.route('/rev/<text>')
+def revert_string(text):
+    return text
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888)
