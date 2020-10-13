934b08aacb0a3e00de820d02b3e3c88bb68e5602
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-10-13 14:54:02 +0200

    ZAD1 Made /rev return reverted text

--- zad1/app.py
+++ zad1/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/rev/<text>')
 def revert_string(text):
-    return text
+    return text[::-1]
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888)
