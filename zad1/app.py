e1fb7d9225db49c3313e60799670fce2cfb476c7
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-10-13 15:09:43 +0200

    ZAD1 Added handler for empty / route

--- zad1/app.py
+++ zad1/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def handle_empty_route():
+    return "W celu uzyskania tekstu w odwrotnej kolejności wpisz adres URL w formacie /rev/<text> gdzie w miejscu <text> podajesz swój ciąg znaków."
+
 @app.route('/rev')
 def handle_incomplete_route():
     return "W celu uzyskania tekstu w odwrotnej kolejności wpisz adres URL w formacie /rev/<text> gdzie w miejscu <text> podajesz swój ciąg znaków."
