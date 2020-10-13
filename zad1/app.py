b1d730e927ea6b0547c04ef96e6936a70de9ec38
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-10-13 15:07:32 +0200

    ZAD1 Added handler for incomplete /rev route

--- zad1/app.py
+++ zad1/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/rev')
+def handle_incomplete_route():
+    return "W celu uzyskania tekstu w odwrotnej kolejności wpisz adres URL w formacie /rev/<text> gdzie w miejscu <text> podajesz swój ciąg znaków."
+
 @app.route('/rev/<text>')
 def revert_string(text):
     return text[::-1]
