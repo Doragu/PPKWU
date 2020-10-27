4a69e9b7937bf0ed52c0bc6b71ac535a36a359ab
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-10-27 14:25:20 +0100

    ZAD2 Changed /validate route to return passed text

--- zad2/app.py
+++ zad2/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/validate')
-def revert_string():
-    return "TEST"
+@app.route('/validate/<text>')
+def revert_string(text):
+    return text
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888, debug=True) 
