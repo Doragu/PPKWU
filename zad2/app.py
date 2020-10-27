a90f0057e2a193275f8a6aae5a840480a9c2dfd5
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-10-27 14:23:18 +0100

    ZAD2 Added /validate route for test and changed app to debug mode

--- zad2/app.py
+++ zad2/app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/validate')
+def revert_string():
+    return "TEST"
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8888) 
+    app.run(host='127.0.0.1', port=8888, debug=True) 
