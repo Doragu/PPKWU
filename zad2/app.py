ce102fd995b22299007d06d40b6c8e6e2b5f9986
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-10-27 14:29:19 +0100

    ZAD2 Added dict for collecting data about passed string

--- zad2/app.py
+++ zad2/app.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+text_params = {
+    "upper": 0,
+    "lower": 0,
+    "numbers": 0,
+    "special": 0
+}
+
 @app.route('/validate/<text>')
 def revert_string(text):
     return text
