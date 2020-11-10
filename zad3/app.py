23ef194490fdd9d25be42461943ca8860f7abf0b
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-11-10 14:26:39 +0100

    ZAD3 Added empty /calendar route

--- zad3/app.py
+++ zad3/app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/calendar')
+def generate_calendar():
+    return "TEST"
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888)  
