dfa48d54b507424e8f11fb0f1c3d2a8de378b435
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-11-10 14:41:48 +0100

    ZAD3 Made API write example .ics file

--- zad3/app.py
+++ zad3/app.py
@@ -3,20 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route('/calendar')
 def generate_calendar():
-    return """BEGIN:VCALENDAR
-    VERSION:2.0
-    PRODID:-//PPKWU//KCH//EN
-    BEGIN:VTODO
-    DTSTAMP:19980130T134500Z
-    SEQUENCE:2
-    DUE:19980415T235959
-    STATUS:NEEDS-ACTION
-    SUMMARY:Submit Income Taxes
-    END:VTODO
-    END:VCALENDAR"""
+    f = open("calendar.ics", "w")
+    
+    content = """BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//PPKWU//KCH//EN\nBEGIN:VTODO\nDTSTAMP:19980130T134500Z\nSEQUENCE:2\nDUE:19980415T235959\nSTATUS:NEEDS-ACTION\nSUMMARY:Submit Income Taxes\nEND:VTODO\nEND:VCALENDAR"""
+    
+    f.write(content)
+    f.close()
+
+    return content
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888)  
