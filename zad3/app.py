91e85887b6f9de91b3161806539bf56db4edc184
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-11-10 14:36:21 +0100

    Made API return example .ics file content

--- zad3/app.py
+++ zad3/app.py
@@ -2,9 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
 @app.route('/calendar')
 def generate_calendar():
-    return "TEST"
+    return """BEGIN:VCALENDAR
+    VERSION:2.0
+    PRODID:-//PPKWU//KCH//EN
+    BEGIN:VTODO
+    DTSTAMP:19980130T134500Z
+    SEQUENCE:2
+    DUE:19980415T235959
+    STATUS:NEEDS-ACTION
+    SUMMARY:Submit Income Taxes
+    END:VTODO
+    END:VCALENDAR"""
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888)  
