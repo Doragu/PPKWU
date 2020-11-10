30f8de82517e15f9950123f7125609eee04ab27f
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-11-10 15:15:40 +0100

    ZAD3 Prepared init algorithm for parsing to .ics file

--- zad3/app.py
+++ zad3/app.py
@@ -3,22 +3,21 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/calendar')
-def generate_calendar():
-    f = open("calendar.ics", "w")
-    
-    content = """BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//PPKWU/ZAD3//EN\nBEGIN:VEVENT\nDTSTART:20201105T170000Z\nDTEND:20201105T180000Z\nSUMMARY:TEST\nEND:VEVENT\nEND:VCALENDAR"""
-    
-    f.write(content)
-    f.close()
-
-    return content
+def get_calendar_events(html):
+    return []
 
 @app.route('/calendar/<year>/<month>')
 def generate_WEEIA_calendar(year, month):
-    request_URI = f"http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok={year}&miesiac={month}&lang=1"
+    cal_content = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//PPKWU/ZAD3//EN\n"
 
-    return requests.get(request_URI).text
+    request_URI = f"http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok={year}&miesiac={month}&lang=1"
+    
+    for event in get_calendar_events(requests.get(request_URI).text):
+        pass
+
+    cal_content += "END:VCALENDAR"
+
+    return cal_content
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888, debug=True)  
