542b68c5de152477831fb1b6ca1190b9a57f83d2
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-12-08 15:39:26 +0100

    ZAD4 Generating content of vcard

--- app/app.py
+++ app/app.py
@@ -12,6 +12,11 @@ class Company:
         self.address = address
         self.phone_number = phone_number
         self.webpage = webpage
+        self.vcf = self.generate_vcf()
+        print(self.vcf + "\n")
+
+    def generate_vcf(self):
+        return f"BEGIN:VCARD\nVERSION:4.0\nORG:{self.name}\nEMAIL:{self.mail}\nADR;TYPE=WORK;LABEL=\"{self.address}\"\nURL:{self.webpage}\nEND:VCARD"
 
 
 def get_values_from_scrapper(html):
@@ -21,19 +26,19 @@ def get_values_from_scrapper(html):
     for item in soup.find_all('li', class_="company-item"):
         temp_soup = BeautifulSoup(str(item), 'html.parser')
         
-        try:
-            print(temp_soup.find("a", class_="company-name").get_text().strip())
-            print(temp_soup.find("a", class_="icon-telephone")["title"])
-            print(temp_soup.find("a", class_="icon-website")["href"])
-            print(temp_soup.find("a", class_="icon-envelope")["data-company-email"])
-            print(temp_soup.find("div", class_="address").get_text().strip())
 
-            companies.append(Company(temp_soup.find("a", class_="company-name").get_text().strip(),
+
+        try:
+
+            company = Company(temp_soup.find("a", class_="company-name").get_text().strip(),
                                     temp_soup.find("a", class_="icon-envelope")["data-company-email"],
                                     temp_soup.find("div", class_="address").get_text().strip(),
                                     temp_soup.find("a", class_="icon-telephone")["title"],
-                                    temp_soup.find("a", class_="icon-website")["href"]
-                            ))
+                                    temp_soup.find("a", class_="icon-website")["href"])
+
+
+
+            companies.append(company)
     
         except:
             pass
@@ -56,6 +61,11 @@ def search():
         get_values_from_scrapper(requests.get(request_URI).text)
 
     return render_template("index.html", companies = get_values_from_scrapper(requests.get(request_URI).text))
+
+
+@app.route('/generate')
+def generate_vcard():
+    return "None"
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888, debug=True)
