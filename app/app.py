2f4426394d193c88faa64f6741ad6c5a0a50b95b
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-12-08 14:30:22 +0100

    ZAD4 Get searched value in flask

--- app/app.py
+++ app/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
+    return render_template("index.html")
+
+@app.route('/search', methods=["POST"])
+def search():
+    if request.method == "POST":
+        data = request.form
+
+        print(data.get("searched"))
+
     return render_template("index.html")
 
 if __name__ == "__main__":
